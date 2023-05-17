f8e447868718a721e5a769765e4638580b798fb0
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-05-17 13:21:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const search_btn = document.getElementById('search_btn');
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'f6dd938eacmsh0d384b1080b54fbp10dd7cjsn5485a7bd7a65',
+		'X-RapidAPI-Key': '{apiKey}',
 		'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com'
 	}
 };
